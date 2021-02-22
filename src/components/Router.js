f8562ecc8d880843ef665a349018e1174e540249
gyuree-kim <gyuree.kim.dev@gmail.com>
2021-02-22 23:51:29 +0900

    2.0 Using Firebase Auth(done), 2.1 Login Form(draft)

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,13 +3,11 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Switch>
         {isLoggedIn ? (
-           //<>: fragment : 여러개를 render하고 싶을 때 사용
           <>
             <Route exact path="/">
               <Home />
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,42 @@
 import React from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (e) => {
+        console.log(e.target.name);
+    };
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+    return (
+        <div>
+            <form >
+                <input 
+                    name="email"
+                    type="text" 
+                    placeholder="Email" 
+                    required 
+                    value={email}
+                />
+                <input 
+                    name="password"
+                    type="password"
+                    placeholder="Password" 
+                    required 
+                    value={password}
+                />
+                <input 
+                    type="submit" 
+                    placeholder="Log In" 
+                    required
+                />        
+            </form>
+            <div>
+                <button>Continue with Google</button>    
+                <button>Continue with Github</button>    
+            </div>  
+        </div>;
+    );
+}
 export default Auth; 
