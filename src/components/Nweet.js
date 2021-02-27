201f7cfb692660671fa4ad98f3dd03a44168ea96
gyuree-kim <gyuree.kim.dev@gmail.com>
2021-02-28 02:56:33 +0900

    3.5 Delete and Update(2)

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import { dbService } from "fbase";
 
 const Nweet = ({nweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+    
+    const onDeleteClick = async () => {
+        const ok =  window.confirm("Are you sure?");
+        if(ok){
+            dbService.doc(`nweets/${nweetObj.id}`).delete();
+        }
+
+    };
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`nweets/${nweetObj.id}`).update({
+            text:newNweet,
+        });
+        setEditing(false);
+    };
+    const onChange = (event) =>{
+        const{ 
+            target: {value},
+        } = event;
+        setNewNweet(value);
+    };
+
     return(
         <div>
-            <h4>{nweetObj.text}</h4>
-            {isOwner && (
+            {editing ? (
                 <>
-                    <button> Delete Nweet </button>
-                    <button> Edit Nweet </button>
+                    <form onSubmit={onSubmit}> 
+                        <input 
+                            type='text' 
+                            placeholder="Edit the text" 
+                            value={newNweet} 
+                            onChange={onChange}
+                            required
+                        />
+                        <input type="submit" value="Update"/>
+                    </form>
+                    <button onClick={toggleEditing}> Cancel </button>
+                </>
+            ) : (
+                <>
+                    <h4>{nweetObj.text}</h4>
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}> Delete Nweet </button>
+                            <button onClick={toggleEditing}> Edit Nweet </button>
+                        </>
+                    )}                
                 </>
             )}
         </div>
